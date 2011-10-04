a0759a44dc521665a6276ed197a2eb7f41ab702d
Keith Beckman <hcgridiron49@gmail.com>
2011-10-04 00:12:39 -0500

    Git -- updating mergetool/difftool wrapper scripts for DiffMerge 3.3.1 x64)

--- Git/git-difftool-diffmerge-wrapper.sh
+++ Git/git-difftool-diffmerge-wrapper.sh
@@ -4,4 +4,4 @@
 # diff is called by git with 7 parameters:
 # path old-file old-hex old-mode new-file new-hex new-mode
 
-"C:/Program Files (x86)/SourceGear/DiffMerge/DiffMerge.exe" "$1" "$2" | cat
+"C:/Program Files/SourceGear/Common/DiffMerge/sgdm.exe" "$1" "$2" | cat
