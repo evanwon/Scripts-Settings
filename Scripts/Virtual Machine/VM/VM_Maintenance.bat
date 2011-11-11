35a72bdee8de3d5689f43f6aea625ae7442b2736
Keith Beckman <hcgridiron49@gmail.com>
2011-11-10 22:31:22 -0600

    2008r2-developer - machine specific settings

--- Scripts/Virtual Machine/VM/VM_Maintenance.bat
+++ Scripts/Virtual Machine/VM/VM_Maintenance.bat
@@ -1,14 +1,24 @@
+net stop mssqlserver
+
 :: Run the DiskCleanup utility...
 cleanmgr.exe /d c:
+cleanmgr.exe /d h:
 
 :: Run disk defrag...
 :: http://technet.microsoft.com/en-us/magazine/ff458356.aspx
 defrag c: /H /U /V
 defrag c: /H /X /U /V
+defrag h: /H /U /V
+defrag h: /H /X /U /V
 
 cd "c:\program files (x86)\sysinternals suite"
 
 :: Run SDelete to zero-out free space...
 sdelete.exe -p 1 -z c:\
+sdelete.exe -p 1 -z h:\
+
+net start mssqlserver
+
+iisreset
 
 pause
