3abc4c4c19f62f481d18d2b8045ac7b0b2cdeef2
keith_beckman <keith_beckman@onlifehealth.com>
2012-03-30 16:57:46 -0500

    PowerShell - adding MongoDB functions

---
+++ Windows/Powershell Library/mongodb-functions.ps1
@@ -0,0 +1,25 @@
+Function Mongo-Install-Service()
+{
+	$logPath 	= $mongoStorage + "\mongo.log"
+	$dataPath 	= $mongoStorage + "\data"
+
+	cd $mongoBinaries
+	
+	.\mongod --logpath $logPath --logappend --dbpath $dataPath --directoryperdb --install --serviceName MongoDB --serviceUser keith_beckman
+	
+	net start MongoDB
+}
+
+Function Mongo-Remove-Service()
+{
+	cd $mongoBinaries
+
+	.\mongod --remove
+}
+
+Function Mongo-Shell()
+{
+	cd $mongoBinaries
+	
+	.\mongo
+}
