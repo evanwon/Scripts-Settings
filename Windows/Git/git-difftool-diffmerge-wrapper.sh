f996e7f3c6ea2237861b45912e809ae3deb9cad0
Keith Beckman <kbeckman.c4sc@gmail.com>
2012-04-08 22:40:03 -0500

    PowerShell - removing Git directory and DiffMerge wrapper contents

--- Windows/Git/git-difftool-diffmerge-wrapper.sh
+++
@@ -1,4 +0,0 @@
-# diff is called by git with 7 parameters:
-# path old-file old-hex old-mode new-file new-hex new-mode
-
-"C:/Program Files/SourceGear/Common/DiffMerge/sgdm.exe" "$1" "$2" | cat
--- Windows/Git/git-mergetool-diffmerge-wrapper.sh
+++
@@ -1,4 +0,0 @@
-# passing the following parameters to mergetool:
-# local base remote merge_result
-
-"C:/Program Files/SourceGear/Common/DiffMerge/sgdm.exe" "$1" "$2" "$3" --result="$4" --title1="Mine" --title2="Merge" --title3="Theirs"
