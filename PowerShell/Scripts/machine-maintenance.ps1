1da037bd192457e70225e79fc401c44c8dc14a45
Keith Beckman <kbeckman.c4sc@gmail.com>
2012-06-07 16:18:27 -0500

    PowerShell - machine-maintenance script comments

--- PowerShell/Scripts/machine-maintenance.ps1
+++ PowerShell/Scripts/machine-maintenance.ps1
@@ -1,10 +1,13 @@
+# Run this script using the following PowerShell call...
+#		.\machine-maintenance.ps1 VM/Host
+
 # define the PowerShell script parameters...
 param($systemType = $(throw "$systemType parameter (VM, Host) is required."))
 
 Clean-Disk
 
 # Add a console-wait because Clean-Disk is a non-shell app and will execute asynchronously without this...
-$pause = Read-Host "Press any key to continue..."
+$pause = Read-Host "Press any key to continue after Clean-Disk finishes..."
 
 if ($systemType.Equals("VM"))
 {
