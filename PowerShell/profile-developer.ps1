181509e275920aacd2e4264092b42ec6fd613f2a
kbeckman <kbeckman.c4sc@gmail.com>
2012-08-18 19:49:29 -0500

    PowerShell - checking for existence of source code dir

--- PowerShell/profile-developer.ps1
+++ PowerShell/profile-developer.ps1
@@ -1,7 +1,11 @@
-$sourceCode		= "S:\SourceCode"
-$codeGitHub		= Join-Path $sourceCode "GitHub"
-$codeCodePlex 	= Join-Path $sourceCode "CodePlex"
-$codeBitBucket	= Join-Path $sourceCode "Bitbucket"
+$sourceCode	= "S:\SourceCode"
 
-$mongoBinaries	= "C:\Program Files\MongoDB\bin"
-$mongoStorage	= "S:\MongoDB_Data"
+if (Test-Path $sourceCode)
+{
+	$codeGitHub		= Join-Path $sourceCode "GitHub"
+	$codeCodePlex 	= Join-Path $sourceCode "CodePlex"
+	$codeBitBucket	= Join-Path $sourceCode "Bitbucket"
+
+	$mongoBinaries	= "C:\Program Files\MongoDB\bin"
+	$mongoStorage	= "S:\MongoDB_Data"
+}
