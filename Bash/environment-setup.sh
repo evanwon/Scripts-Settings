4d90b2018ef24d5ab833d9b1ef7af86de522f141
kbeckman <kbeckman.c4sc@gmail.com>
2013-02-19 18:12:28 -0600

    Bash - adding environment-setup.sh script for a single script source call in .bashrc or .zshrc

---
+++ Bash/environment-setup.sh
@@ -0,0 +1,11 @@
+# http://stackoverflow.com/questions/11785126/cfurlcopyresourcepropertyforkey-failed-because-it-was-passed-this-url-which-has
+# fixes a gitk issue in oh-my-zsh
+alias gitk='gitk 2>/dev/null'
+
+
+# Scripts-Settings
+. ~/Scripts-Settings/Bash/FuncLib/functions-utility.sh
+. ~/Scripts-Settings/Bash/FuncLib/functions-git.sh
+. ~/Scripts-Settings/Bash/FuncLib/functions-openssl.sh
+
+
