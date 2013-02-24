dd67aec3ee77ffc1edd438aa5cbd40f1af455f1b
kbeckman <kbeckman.c4sc@gmail.com>
2013-02-24 15:28:15 -0600

    PowerShell - removing profile-developer.ps1

--- PowerShell/profile-developer.ps1
+++
@@ -1,8 +0,0 @@
-$sourceCode	= "S:\SourceCode"
-
-if (Test-Path $sourceCode)
-{
-	$codeGitHub		= Join-Path $sourceCode "GitHub"
-	$codeCodePlex 	= Join-Path $sourceCode "CodePlex"
-	$codeBitBucket	= Join-Path $sourceCode "Bitbucket"
-}
